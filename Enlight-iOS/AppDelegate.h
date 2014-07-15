3371a422f9b463724e603eba403d54e750aa3948
Kenneth S <kksiu@wisc.edu>
2014-07-14 22:42:29 -0500

    Added Project

---
+++ Enlight-iOS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Enlight-iOS
+//
+//  Created by Kenneth Siu on 7/14/14.
+//  Copyright (c) 2014 Enlight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Enlight-iOS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Enlight-iOS
+//
+//  Created by Kenneth Siu on 7/14/14.
+//  Copyright (c) 2014 Enlight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
