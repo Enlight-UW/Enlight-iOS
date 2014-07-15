b25795bbca3f64ee116a0a8984d8056db1f30a82
Kenneth S <kksiu@wisc.edu>
2014-07-14 22:43:11 -0500

    Added Project file

---
+++ Enlight-iOSTests/Enlight_iOSTests.m
@@ -0,0 +1,34 @@
+//
+//  Enlight_iOSTests.m
+//  Enlight-iOSTests
+//
+//  Created by Kenneth Siu on 7/14/14.
+//  Copyright (c) 2014 Enlight. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Enlight_iOSTests : XCTestCase
+
+@end
+
+@implementation Enlight_iOSTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
