354998bc4d042e0f0301329c0c42568fe1ea99b9
Kenneth S <kksiu@wisc.edu>
2014-07-16 00:07:09 -0500

    Added switches with labels that turn color when on

---
+++ Enlight-iOS/MainViewController.h
@@ -0,0 +1,19 @@
+//
+//  MainViewController.h
+//  Enlight-iOS
+//
+//  Created by Kenneth Siu on 7/15/14.
+//  Copyright (c) 2014 Enlight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+//will contain all the switches when setting the fountain
+@property NSMutableArray *switchArray;
+
+//contains all the labels for the switches
+@property NSMutableArray *labelSwitchArray;
+
+@end
